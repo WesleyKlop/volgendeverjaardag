0e4548fa6d5586ad8490521a4fdb0a9f9d225220
Wesley Klop <wesley19097@gmail.com>
2022-08-18 17:49:09 +0200

    Fix

--- .github/scripts/version.mjs
+++ .github/scripts/version.mjs
@@ -31,9 +31,12 @@ export default async function determineNextVersion({ context, core, github }) {
       repo: context.repo.repo,
     })
     .catch((r) => r);
+
   if (response.status !== 200) {
     console.log("No previous release found, going to v1.0.0");
-    return "v1.0.0";
+    core.setOutput("tag", "v1.0.0");
+    core.setOutput("name", "Release v1.0.0");
+    return;
   }
 
   const currentVersion = tag_name
