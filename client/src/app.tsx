a4404193f1928a0c1189126a1af69fc21e91d8ad
Wesley Klop <wesley19097@gmail.com>
2022-08-21 00:26:22 +0200

    Improve Dockerfiles

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,14 @@
-import * as React from 'react'
+import React, { StrictMode } from 'react'
 import { App } from './app/App'
 import { createRoot } from 'react-dom/client'
 
 const $app = document.querySelector<HTMLDivElement>('#app')!
 
 const root = createRoot($app)
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
 export {}
