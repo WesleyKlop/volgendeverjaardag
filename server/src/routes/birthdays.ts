fbf7962fe7439000bb33a435565bdefc9847f88a
Wesley Klop <wesley19097@gmail.com>
2022-09-23 19:10:34 +0200

    Who needs validation anyway

--- server/src/routes/birthdays.ts
+++ server/src/routes/birthdays.ts
@@ -29,44 +29,68 @@ export const registerBirthDays = (server: Server) => {
   //     }),
   // );
 
-  server.get(
-    "/api/birthdays/:code/next",
-    res("json"),
-    (ctx) =>
-      withConnection(async (client) => {
-        const { code } = ctx.params;
+  server.get("/api/birthdays/:code/next", res("json"), (ctx) =>
+    withConnection(async (client) => {
+      const { code } = ctx.params;
 
-        if (typeof code !== "string") {
-          ctx.res.body = {
-            message: "Invalid code",
-          };
-          ctx.res.status = 404;
-          return;
-        }
-        const nextBirthday = await findNextByCode(code, client);
+      if (typeof code !== "string") {
+        ctx.res.body = {
+          message: "Invalid code",
+        };
+        ctx.res.status = 404;
+        return;
+      }
+      const nextBirthday = await findNextByCode(code, client);
 
-        if (!nextBirthday) {
-          ctx.res.status = 404;
-          return;
-        }
+      if (!nextBirthday) {
+        ctx.res.status = 404;
+        return;
+      }
 
-        ctx.res.body = nextBirthday;
-        ctx.res.status = 200;
-      }),
+      ctx.res.body = nextBirthday;
+      ctx.res.status = 200;
+    })
   );
 
-  server.post(
-    "/api/birthdays",
-    req("json"),
-    res("json"),
-    (ctx) =>
-      withConnection(async (client) => {
-        const response = await createBirthDay(ctx.body, client);
-        if (!response) {
-          throw "Failed to save birthday";
-        }
+  server.post("/api/birthdays", req("json"), res("json"), (ctx) =>
+    withConnection(async (client) => {
+      const birthday = {
+        code: ctx.body.code,
+        birthDate: new Date(ctx.body.birthDate),
+        name: ctx.body.name,
+      };
+      if (
+        typeof birthday.code !== "string" ||
+        birthday.code.length < 6 ||
+        birthday.code.length >= 32
+      ) {
+        ctx.res.status = 422;
+        ctx.res.body = {
+          message: "Invalid code, must be a string between 6 and 32 characters",
+        };
+        return;
+      }
+      if (isNaN(birthday.birthDate.getTime())) {
+        ctx.res.status = 422;
+        ctx.res.body = {
+          message: "Invalid birthdate, must be a valid date",
+        };
+        return;
+      }
+      if (typeof birthday.name !== "string" || birthday.name.length >= 32) {
+        ctx.res.status = 422;
+        ctx.res.body = {
+          message: "Invalid name, must be a string smaller than 32 characters",
+        };
+        return;
+      }
 
-        ctx.res.body = response;
-      }),
+      const response = await createBirthDay(birthday, client);
+      if (!response) {
+        throw "Failed to save birthday";
+      }
+
+      ctx.res.body = response;
+    })
   );
 };
