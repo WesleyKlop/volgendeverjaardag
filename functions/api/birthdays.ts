282643bcf20a0d30c187ccd07c0fb523cb855c2f
Wesley Klop <wesley19097@gmail.com>
2024-09-29 10:21:11 +0200

    Fix type conversion bug

--- functions/api/birthdays.ts
+++ functions/api/birthdays.ts
@@ -81,7 +81,7 @@ export async function onRequestPost(ctx: EventContext<Env, never, never>) {
       birthday.code,
       birthday.name,
       birthday.birth_date.getTime() / 1000,
-      birthday.website,
+      birthday.website?.toString(),
       birthday.species,
     )
     .run()
