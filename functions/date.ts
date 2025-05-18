b394c2328f8ec9ad8ba9714970c23b8c1b4ab691
Wesley Klop <wesley19097@gmail.com>
2025-05-18 14:19:08 +0200

    Normalize docblocks

--- functions/date.ts
+++ functions/date.ts
@@ -1,14 +1,14 @@
-// Formats a JavaScript Date object to a string in the format YYYY-MM-DD
+/** Formats a JavaScript Date object to a string in the format YYYY-MM-DD */
 export function formatISODate(date: Date): string {
   return date.toISOString().split('T')[0]
 }
 
-// Returns true if the given string is a valid ISO date in the format YYYY-MM-DD
+/** Returns true if the given string is a valid ISO date in the format YYYY-MM-DD */
 export function isValidISODate(date: string): boolean {
   return /^\d{4}-\d{2}-\d{2}$/.test(date) == true && isNaN(Date.parse(date)) == false
 }
 
-// Returns the age in years between the given birth date and the other date
+/** Returns the age in years between the given birth date and the other date */
 export function calculateAgeInYears(birthDate: Date, otherDate: Date = new Date()): number {
   const years = otherDate.getUTCFullYear() - birthDate.getUTCFullYear()
 
@@ -23,7 +23,7 @@ export function calculateAgeInYears(birthDate: Date, otherDate: Date = new Date(
   return years
 }
 
-// Returns the next birthday after the given date
+/** Returns the next birthday after the given date */
 export function calculateNextBirthday(birthDate: Date, today: Date): Date {
   const nextBirthDay = new Date(
     Date.UTC(today.getUTCFullYear(), birthDate.getUTCMonth(), birthDate.getUTCDate()),
